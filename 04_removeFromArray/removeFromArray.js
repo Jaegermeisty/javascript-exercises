c8ff617d793f49bb03bd2d9fc45e61e9a7500773
Mathias JP <175939829+Jaegermeisty@users.noreply.github.com>
2025-02-14 12:17:43 +0100

    Did ex. 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...valuesToRemove) {
+  const newArray = [];
+
+  array.forEach(item => {
+    if (!valuesToRemove.includes(item)) {
+      newArray.push(item);
+    }
+  });
+
+  return newArray;
 };
 
 // Do not edit below this line
