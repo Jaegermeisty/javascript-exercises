486712e7d800aa85657236bb67a585a307fe6732
Mathias JP <175939829+Jaegermeisty@users.noreply.github.com>
2025-02-14 12:44:48 +0100

    Finished ex. 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+  if (a > b) {
+    [a, b] = [b, a];
+  }
 
+  if (isNaN(a) || isNaN(b) || !Number.isInteger(a) || !Number.isInteger(b)
+    || a < 0 || b < 0) {
+    return 'ERROR';
+  }
+
+  let sum = 0;
+
+  for (i = a; i <= b; i++) {
+    sum += i;
+  }
+  return sum;
 };
 
 // Do not edit below this line
